b3c79111742d9036c1819d1cc7b7601b76256b36
salah137 <boksaloh.com>
2024-07-04 16:15:40 +0100

    finished

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,61 +1,52 @@
 
 "use client"
 import { Inter } from "next/font/google";
-import "./style.css";
+import { useRouter } from "next/navigation";
 import { SiCoursera } from "react-icons/si";
 import { BiNews } from "react-icons/bi";
-import { useRouter } from "next/navigation"
+import "./style.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <html lang="en">
-      <head></head>
-      <body className={inter.className}>
-        <div className="main">
-          <div className="nav">
-            <div onClick={
-              ()=>{
-                router.push("/home/tutorials")
-              }
-            }>
-              <SiCoursera />
-              <h2>Tutorials</h2>
-            </div>
-
-            <div onClick={
-                       ()=>{
-                        router.push("/home/annonces")
-                      }
-            }>
-              <BiNews />
-              <h2>News</h2>
-            </div>
-            
-            <div onClick={
-              localStorage.removeItem()
-                       ()=>{
-                        localStorage.removeItem("email")
-                        router.push("/")
-                      }
-            }>
-              <BiNews />
-              <h2>Log Out</h2>
-            </div>
-            
-          </div>
-          {children}
-
+    <div className={`main ${inter.className}`}>
+      <div className="nav">
+        <div
+          onClick={() => {
+            router.push("/home/tutorials");
+          }}
+        >
+          <SiCoursera />
+          <h2>Tutorials</h2>
         </div>
-      </body>
-    </html>
+
+        <div
+          onClick={() => {
+            router.push("/home/annonces");
+          }}
+        >
+          <BiNews />
+          <h2>News</h2>
+        </div>
+
+        <div
+          onClick={() => {
+            localStorage.removeItem("email");
+            router.push("/");
+          }}
+        >
+          <BiNews />
+          <h2>Log Out</h2>
+        </div>
+      </div>
+      {children}
+    </div>
   );
 }
