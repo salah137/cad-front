19e999474b7b115e1ae6bfccf91cd58a012975c7
salah137 <boksaloh.com>
2024-07-14 16:46:05 +0100

    finish

--- app/home/tutorials/page.tsx
+++ app/home/tutorials/page.tsx
@@ -1,18 +1,20 @@
 "use client"
 
 import Image from 'next/image'
-import logo from "../../assets/images/cad-logo.png"
 import Link from 'next/link'
+import design from "../../assets/images/design.jpeg"
+import art from "../../assets/images/art.jpeg"
+
 export default function page() {
     return <main className='tutorials'>
         <div className="chose-tuto">
             <Link href={"/home/tutorials/design"} className='tut-elem-design'>
-                <Image src={logo} alt="logo" className='logo-tuto' />
+                <Image src={design} alt="logo" className='logo-tuto' />
                 <h3>Designe</h3>
             </Link>
 
             <Link href={"/home/tutorials/art"} className='tut-elem-art'>
-                <Image src={logo} alt="logo" className='logo-tuto' />
+                <Image src={art} alt="logo" className='logo-tuto' />
                 <h3>Art</h3>
             </Link>
 
